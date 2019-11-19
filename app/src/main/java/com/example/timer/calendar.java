8351387b3cac6a57b848b4fa02178e141d36a3f3
Joseph Saltalamacchia <jks6634@rit.edu>
2019-11-19 16:56:27 -0500

    Added mobility

--- app/src/main/java/com/example/timer/calendar.java
+++ app/src/main/java/com/example/timer/calendar.java
@@ -1,14 +1,37 @@
 package com.example.timer;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class calendar extends AppCompatActivity {
+
+    Button homeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        homeButton = findViewById(R.id.Home);
+
+        homeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                goHome();
+            }
+        });
     }
+
+    public void goHome()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
