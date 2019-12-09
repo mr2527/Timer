b6d097e5313e83a8065e04d8b5294cdd27679c10
st5626 <st5626@rit.edu>
2019-12-08 22:04:29 -0500

    Can now create a Task using the data that user has entered

--- app/src/main/java/com/example/timer/add_task.java
+++ app/src/main/java/com/example/timer/add_task.java
@@ -6,12 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Date;
 
 /**
  * Window for adding a task, prompts the user to enter the name, the due date, and the
  * description of the task
  */
 public class add_task extends AppCompatActivity {
+
+    String taskName, taskDescription, taskDueDate;
+
+    EditText taskNameInput;
+    EditText taskDescriptionInput;
+    EditText taskDueDateInput;
 
     private Button createTask;
 
@@ -27,11 +36,17 @@ public class add_task extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
 
+        taskNameInput = findViewById(R.id.taskName);
+        taskDescriptionInput = findViewById(R.id.taskDescription);
+        taskDueDateInput = findViewById(R.id.taskDueDate);
         createTask = findViewById(R.id.createTask);
         createTask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createTask();
+                taskName = taskNameInput.getText().toString();
+                taskDescription = taskDescriptionInput.getText().toString();
+                taskDueDate = taskDueDateInput.getText().toString();
+                createTask(taskName, taskDescription, taskDueDate);
             }
         });
 
@@ -48,8 +63,9 @@ public class add_task extends AppCompatActivity {
      * Creates the task that the user has entered.
      * @author Joseph Saltalamacchia
      */
-    public void createTask()
+    public void createTask(String name, String description, String date)
     {
+        com.example.timer.ui.Tasks.Task newTask = new com.example.timer.ui.Tasks.Task(name, description, date);
         startActivity(new Intent(this, Task.class));
     }
 
