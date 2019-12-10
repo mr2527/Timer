8e05c5b9ef098612b91331015b20b52c9ae5239d
Joseph Saltalamacchia <jks6634@rit.edu>
2019-12-10 11:13:11 -0500

    Got user input and output working

--- app/src/main/java/com/example/timer/add_task.java
+++ app/src/main/java/com/example/timer/add_task.java
@@ -8,7 +8,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.Buffer;
 import java.util.Date;
+import java.io.File;
 
 /**
  * Window for adding a task, prompts the user to enter the name, the due date, and the
@@ -26,16 +33,28 @@ public class add_task extends AppCompatActivity {
 
     private Button cancel;
 
+    private BufferedWriter out;
+
+    private BufferedReader in;
+
     /**
      * auto-generated method that creates what the user sees/creates the application
      * @param savedInstanceState
-     * @author Su Thit Thazin
+     * @author Su Thit Thazin $ Joseph Saltalamacchia
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
 
+        //create the readers and writers
+
+        try {
+            out = new BufferedWriter(new FileWriter(new File(getApplicationContext().getFilesDir() + "Tasks.txt")));
+            in = new BufferedReader(new FileReader(new File(getApplicationContext().getFilesDir() + "Tasks.txt")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         taskNameInput = findViewById(R.id.taskName);
         taskDescriptionInput = findViewById(R.id.taskDescription);
         taskDueDateInput = findViewById(R.id.taskDueDate);
@@ -47,9 +66,19 @@ public class add_task extends AppCompatActivity {
                 taskDescription = taskDescriptionInput.getText().toString();
                 taskDueDate = taskDueDateInput.getText().toString();
                 createTask(taskName, taskDescription, taskDueDate);
+                try {
+                    out.write(taskName + ", "  + taskDescription + ", " + taskDueDate);
+                    out.flush();
+                    String words = in.readLine();
+                    System.out.println(words);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
         });
 
+        System.out.println("Anything at all");
         cancel = findViewById(R.id.cancel);
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
